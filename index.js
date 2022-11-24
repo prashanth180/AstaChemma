183f0adaf693855eef0e0ad1dca49e6dbed3fb03
prashanth180 <a.prashanth18@gmail.com>
2022-11-24 09:28:55 +0530

    Adds totalActiveTokens logic

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ var tokens = { "p1": { 1: false, 2: false, 3: false, 4: false }, "p2": { 1: fals
 // tokenPosition value needs to match value on board
 var tokenPosition = { "p1": { 1: '', 2: '', 3: '', 4: '' }, "p2": { 1: '', 2: '', 3: '', 4: '' }, "p3": { 1: '', 2: '', 3: '', 4: '' }, "p4": { 1: '', 2: '', 3: '', 4: '' } };
 
+
 // IT STARTS HERE
 // This method is called for every chance of user
 function playUser() {
   var activePlayer = getActivePlayer();
   var diceVal = playDice(activePlayer);
-  if (diceVal == 4 || diceVal == 8) {
+  var totalActiveTokens = totalActiveTokens();
+  if (totalActiveTokens < 4 && (diceVal == 4 || diceVal == 8)) {
     // check if user likes to add a new token as active, if active return else continue to next lines
   }
 
   chosenToken = chooseActiveToken //choose an active token to move the card further based on value
   moveChosenTokenOnBoard(activePlayer, chosenToken, diceVal);
+}
+
+function totalActiveTokens(activePlayer) {
+  token[activePlayer]
+
+  //returns number of active tokens in token[activePlayer]
+  return 0;
 }
 
 // Board, token
