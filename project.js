b153628dc0c5153e295aa14e5765e7e2936bdfc2
Manoj7520 <manojkumar.g636@gmail.com>
2022-11-27 12:13:29 +0530

    I work on Dice and Tokens

--- project.js
+++ project.js
@@ -1,27 +1,69 @@
-var random;
+var dice;
+var variable=1;
 var num;
 sides=5;
-function printNumber(){
-  random= Math.floor(Math.random()*sides)+1;
+var z1,z2,z3,z4;
+function Dice(){
+  dice= Math.floor(Math.random()*sides)+1;
   num=document.getElementById("print");
-  if(random==5){
+  if(dice==5){
     num.innerHTML="Number"+":"+"8";
    }
    else{
-    num.innerHTML="Number"+":"+random;
+    num.innerHTML="Number"+":"+dice;
    }
 }
-var tokens = {
-"p1": { 1: false, 2: false, 3: false, 4: false }, 
-"p2": { 1: false, 2: false, 3: false, 4: false }, 
-"p3": { 1: false, 2: false, 3: false, 4: false },
-"p4": { 1: false, 2: false, 3: false, 4: false } };
-
-function func26(){
-    if(random==8 || random==4 && this.p1["1"]==false){
-        token=document.getElementById("e6").value="xa";
+ token1=true;
+ x1=["💴"];
+ o1=["⚡"];
+function playerone(){
+    if( dice==5 || dice==4 ||variable==4 && token1==true ){
+        z=document.getElementById("e3").value="💴";
+        z1=document.getElementById("e6").value=x1;
+        variable++;
+        token1=false;
+     }
+     if(variable>5){
+      z=document.getElementById("e3").value=" ";
+      z=document.getElementById("e6").value=" ";
+      z1=document.getElementById("print").innerHTML="no tokens";
+      user1=true;
     }
-    else {
-        token=document.getElementById("e6").value="x";
+}
+user1=true;
+function func23(){
+  
+        if(dice==1 && token1==false){
+     e4=document.getElementById("e4").value=x1;
+      z=document.getElementById("e3").value=" ";
+      z1=document.getElementById("e6").value=" ";
+      token1=true;
     }
+   else if(dice==2 && token1==false){  
+        e5=document.getElementById("e5").value=x1;
+        z=document.getElementById("e3").value=" ";
+        z1=document.getElementById("e6").value=" ";
+        token1=true;
+    }
+    else if(dice==3 && token1==false){
+      z=document.getElementById("e3").value=" ";
+     d5=document.getElementById("d5").value=x1;
+     z1=document.getElementById("e6").value=" ";
+     token1=true;
+    }
+    else  if(dice==4 && token1==false){
+      z=document.getElementById("e3").value=" ";
+      c5=document.getElementById("c5").value=x1;  
+      z1=document.getElementById("e6").value=" ";
+      token1=true;
+    }
+    else if(dice==5 && token1==false){
+      z=document.getElementById("e3").value=" ";
+      a3=document.getElementById("a3").value=x1;
+      z1=document.getElementById("e6").value=" ";
+      token1=true;
+    }
+}
+function func3(){
+  
 }
