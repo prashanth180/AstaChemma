b2d96dc0862ded686e9e51ef42036f1d180f3c61
Manoj7520 <manojkumar.g636@gmail.com>
2023-09-12 22:25:13 +0530

    added permissions for all tokens

--- project.js
+++ project.js
@@ -12,46 +12,71 @@ var user1=true;
 var user2=true;
 active_tokens = {'e3': [false, false, false, false]}
 //on initial load below are the positions of the coins
-present_position= {'e3':['e3','e3','e3','e3']}
+present_position= {'e3':[null, null, null, null]}
+
+function getActiveTokensOf(house){
+  active_tokens_of = [];
+  for(let i =0;i<4;i++){
+    if(active_tokens[house][i]){
+      active_tokens_of.push(i);
+    }
+  }
+  return active_tokens_of;
+}
+function getAllTokensPresentAt(house, position){
+  tokens_at_position = [];
+  for(let i =0;i<4;i++){
+    if(present_position[house][i] == position){
+      tokens_at_position.push(i);
+    }
+  }
+  console.log(tokens_at_position);
+  return tokens_at_position;
+}
 function Dice(){
   const values = [1, 2, 3, 4, 8];
   const randomIndex = Math.floor(Math.random() * values.length);
   const dice = values[randomIndex];
   let d=document.getElementById("print");
   d.innerHTML="Number"+":"+dice;
-  
-  if( dice==8 && user1==true || token1==true && active_tokens['e3'][0]==true){
-    
-    e6=document.getElementById('e3').value="🐵1"
-    user1=false 
-    token1=false
-    
+  chosen_token = null;
+  if(active_tokens['e3'].includes(true)){
+    tokens = getActiveTokensOf('e3');
+    var r = prompt(`choose between ${tokens}`);
+    //add validations to check if the token is taken from given tokens only
+    chosen_token = parseInt(r);
   }
 
-else if(user1==false || token1==false  && active_tokens['e3'][0]==false){ 
-  old_position = present_position['e3'][0];
-  present_index_of_e3 = PATH1.indexOf(present_position['e3'][0]);
-  new_index_of_e3 = (present_index_of_e3 + dice) < PATH1.length ? present_index_of_e3 + dice : null
-  if(new_index_of_e3 == null){
-    alert("entered dice value exceeds limit");
-    return present_index_of_e3;
+  if (dice == 8 && active_tokens['e3'].includes(false) && confirm("Choose new token?")) {
+    new_token = active_tokens['e3'].indexOf(false);
+    active_tokens['e3'][new_token] = true;
+    present_position['e3'][new_token] = 'e3';
+    chosen_token = new_token;
+    document.getElementById('e3').value=`🐵${getAllTokensPresentAt('e3', 'e3').join()}`
+  }else if(chosen_token!=null){
+    console.log(typeof chosen_token);
+    old_position = present_position['e3'][chosen_token];
+    present_index_of_e3 = PATH1.indexOf(present_position['e3'][chosen_token]);
+    new_index_of_e3 = (present_index_of_e3 + dice) < PATH1.length ? present_index_of_e3 + dice : null
+    if(new_index_of_e3 == null){
+      alert("entered dice value exceeds limit");
+      return present_index_of_e3;
+    }
+    console.log(`Moved from ${present_index_of_e3} to ${new_index_of_e3} on dice value of ${dice}`);
+    console.log(`Moved from ${present_position['e3'][chosen_token]} to ${PATH1[new_index_of_e3]} on dice value of ${dice}`);
+    old_position = present_position['e3'][chosen_token];
+    present_position['e3'][chosen_token] = PATH1[new_index_of_e3];
+    old_position_items = getAllTokensPresentAt('e3',  old_position);
+    new_position_items = getAllTokensPresentAt('e3',  present_position['e3'][chosen_token]);
+    document.getElementById(old_position).value= old_position_items.length == 0 ? "" : `🐵${old_position_items.join()}`;
+    document.getElementById(present_position['e3'][chosen_token]).value=`🐵${new_position_items.join()}`;
+    d.innerHTML="Number"+":"+dice;
+    if(new_index_of_e3==PATH1.length-1){
+      alert("congrats");
+    }o1="🦁";
   }
-  console.log(`Moved from ${present_index_of_e3} to ${new_index_of_e3} on dice value of ${dice}`);
-  console.log(`Moved from ${present_position['e3'][0]} to ${PATH1[new_index_of_e3]} on dice value of ${dice}`);
-  present_position['e3'][0] = PATH1[new_index_of_e3];
-  document.getElementById(old_position).value="";
-  document.getElementById(present_position['e3'][0]).value="🐵1";
-  d.innerHTML="Number"+":"+dice;
-  if(new_index_of_e3==PATH1.length-1){
-    alert("congrats");
-  }
-  user1=false
-  token1=true
-}
-}
-//  x1="🐵";
-//  o1="🦁";
 function playerone(){
     
 }
 
+  }
